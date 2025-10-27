d8bccc6dab62bba88349645c701f64855aa20251
Ante Pavlović <74955024+AntePavlovic@users.noreply.github.com>
2025-10-27 16:22:48 +0100

    Update Supabase client configuration
    Replaced Supabase URL and key with placeholders.

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://klnspuosgxokqsjopwer.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsbnNwdW9zZ3hva3Fzam9wd2VyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTA3MDQ3MiwiZXhwIjoyMDY0NjQ2NDcyfQ.LWI6LfSsPzvkgsCSw_SZCqJ4CtZx7Pmukcyp9NB3IOM';
+const supabaseUrl = '/..';
+const supabaseKey = '/..';
 export const supabase = createClient(supabaseUrl, supabaseKey);
